2d6defc40c62ce673e1d6f8f488b3d92ff58b2a3
片田 猛士 <katada-t@hi-ad.jp>
2019-03-08 13:00:41 +0900

    blockgame.py稼働確認

--- blockgame.py
+++ blockgame.py
@@ -35,37 +35,42 @@ while endflag == 0:
     x = WIDTH / 2
     y = HEIGHT - 64
     paddle = pygame.Rect(x - (paddlew / 2),y - (paddleh / 2), paddlew,paddleh)
+    #ブロックを作る
     blocks = []
     for i in range(50):
         x = (i % 10) * (blockw + 4) + 64
-        y = int(i / 10) * (blockw + 4) + 64
+        y = int(i / 10) * (blockh + 4) + 64
         blocks.append(pygame.Rect(x, y, blockw, blockh))
     gameover = 0
     while endflag == 0:
         for event in pygame.event.get():
             if event.type == pygame.QUIT: endflag = 1
+        #キー入力の取得
         press = pygame.key.get_pressed()
         x = paddle.centerx
         if(press[pygame.K_LEFT ]): x -= 8
         if(press[pygame.K_RIGHT ]): x += 8
         if x >= (paddlew / 2) and x <= (WIDTH - (paddlew / 2)):
             paddle.centerx = x
-        x = bally + bx1
+        x = ballx + bx1
         y = bally + by1
+        """画面枠に対してのボールの反射"""
         if x < br or x > (WIDTH - br): bx1 = -bx1
         if y < br: by1 = -by1
         if y > HEIGHT: gameover += 1
+        """パドルに対してのボールの反射"""
         dx = paddle.centerx - x
-        de = paddle.centery - y
+        dy = paddle.centery - y
         if dy == 0: dy = 1
         if abs(dx) < (paddlew / 2 + br) and abs(dy) < (paddleh / 2 + br):
             if abs(dx /dy) > (paddlew / paddleh):
                 bx1 = -bx1
-                ballx = paddle.centerx - sgn(dx) * (paddlew/2 : br)
+                ballx = paddle.centerx - sgn(dx) * (paddlew/2 + br)
             else:
                 bx1 = -dx / 10
                 by1 = -by1
                 bally = paddle.centery - sgn(dy) * (paddleh/2 + br)
+        """ブロックに対してのボールの反射"""
         for block in blocks:
             dx = block.centerx - x
             dy = block.centery - y
@@ -77,15 +82,18 @@ while endflag == 0:
                 else:
                     by1 = -by1
                     bally = block.centery - sgn(dy) * (blockh / 2 + br)
-                blocks/remove(block)
+                blocks.remove(block)
                 break
         ballx += bx1
         bally += by1
         screen.fill(BLUE)
+        """ブロックの描写"""
         for block in blocks:
             color = colortable[int(block.y / 28) % 3]
             pygame.draw.rect(screen,color,block)
+        """パドルの描写"""
         pygame.draw.rect(screen,WHITE,paddle)
+        """ゲームオーバー時の処理"""
         pygame.draw.circle(screen,WHITE,(int(ballx), int(bally)), br)
         if gameover > 0:
             imagetext = myfont.render("GAME OVER", True, WHITE)
